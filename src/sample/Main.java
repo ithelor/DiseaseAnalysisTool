74ce0fe8c742990bae3808aea7f4208a27c1d298
unitofWelch <61781601+unitofWelch@users.noreply.github.com>
2021-04-06 22:19:35 +1000

    3rd design version // layout almost complete

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,8 +3,8 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        ScrollPane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Pane root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         Scene scene = new Scene(root, 820, 600); //               <-- \
 //        stage.setMinWidth(640); stage.setMinHeight(560);     // doesn't match --> /
